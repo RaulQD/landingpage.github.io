47e5d5723929ddd5ec58d45d2dcc819cab790f46
Raul Quispe Diaz <raul.g.quispe@gmail.com>
2022-03-02 09:17:12 -0500

    agregando nuevos iconos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 const navbarMenu = document.querySelector('.navbar__menu');
 const openMenu = document.querySelector('.openMenu');
-
+const header = document.querySelector('nav');
 
 openMenu.addEventListener('click',() => {
     navbarMenu.classList.toggle('show');
 });
+
+function scrollHeader(){
+  if(this.scrollY >= 50){
+    header.classList.add('scroll-nav');
+  }else{
+    header.classList.remove('scroll-nav');
+  }
+}
+window.addEventListener('scroll',scrollHeader);
